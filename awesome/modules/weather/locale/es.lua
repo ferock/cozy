5f08687ad3f5cad1971ac793539835348b53395e
ferockArch <ferock07@gmail.com>
2023-01-18 10:34:25 -0600

    This changes are custom

---
+++ awesome/modules/weather/locale/es.lua
@@ -0,0 +1,14 @@
+local es = {
+    warning_title = "Clima Widget",
+    parameter_warning = "Los parametros requeridos no han sido configurados: ",
+    directions = {
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW",
+        "WSW", "W", "WNW", "NW", "NNW", "N"
+    },
+    feels_like = "Sensasion Termica ",
+    wind = "Viento: ",
+    humidity = "Humedad: ",
+    uv = "UV: "
+}
+
+return es
